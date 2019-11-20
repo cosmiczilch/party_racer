f503df83ffd25e81844cd9735bb724775af13b00
Avi Krishnan <cosmiczilch@gmail.com>
2019-11-20 22:33:39 +0530

    Don't leave room automatically when other player leaves

--- Assets/Shared/Scripts/Multiplayer/MultiPlayerManager.cs
+++ Assets/Shared/Scripts/Multiplayer/MultiPlayerManager.cs
@@ -83,6 +83,7 @@ namespace TimiMultiPlayer {
         public void LeaveRoom() {
             this._pendingRoomJoinRequest = null;
             if (PhotonNetwork.IsConnected && PhotonNetwork.CurrentRoom != null) {
+                DebugLog.LogColor("Leaving photon room", LogColor.blue);
                 PhotonNetwork.LeaveRoom();
             }
         }
@@ -181,9 +182,6 @@ namespace TimiMultiPlayer {
         public override void OnPlayerLeftRoom(Player otherPlayer) {
             DebugLog.LogColor("Player left room: " + otherPlayer.ActorNumber.ToString(), LogColor.green);
             OnOtherPlayerLeftRoom.Invoke();
-
-            // We should leave the room as well to prevent new players searching for rooms from joining this room
-            this.LeaveRoom();
         }
         #endregion
 
